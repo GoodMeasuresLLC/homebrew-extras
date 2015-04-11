3579f5d88596388dff8f1f3d50a8bbffc4874eea
Robert Mathews <rob@justsoftwareconsulting.com>
2015-04-11 13:01:11 -0400

    update git-woe version, maybe with correct SHA?

--- git-woe.rb
+++ git-woe.rb
@@ -2,7 +2,7 @@ class GitWoe < Formula
   homepage ""
   url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.1.tar.gz"
   version "1.0.1"
-  sha1 "c68d2453171cc34f35367c2cdde84ca2aaa4ad9a"
+  sha1 "9394a521dc534abc344997b94b8ce9d96d779a62"
 
   def install
     bin.install "git-woe"
