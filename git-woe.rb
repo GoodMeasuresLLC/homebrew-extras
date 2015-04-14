effeb9bb5885af20f2315ffb629f38428ed65204
Robert Mathews <rob@justsoftwareconsulting.com>
2015-04-14 09:33:05 -0400

    update

--- git-woe.rb
+++ git-woe.rb
@@ -1,8 +1,8 @@
 class GitWoe < Formula
   homepage ""
-  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.1.tar.gz"
-  version "1.0.1"
-  sha1 "9394a521dc534abc344997b94b8ce9d96d779a62"
+  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.2.tar.gz"
+  version "1.0.2"
+  sha1 "f6485f6e95577b2eebe4d2460bf231332158165f"
 
   def install
     bin.install "git-woe"
