0f9c2d0454d8fd79d6f50ffcc33966e3cdd67f49
Robert Mathews <rob@justsoftwareconsulting.com>
2015-07-27 11:21:40 -0400

    fixed stuff harder

--- git-woe.rb
+++ git-woe.rb
@@ -1,6 +1,6 @@
 class GitWoe < Formula
   homepage ""
-  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.3.tar.gz"
+  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.4.tar.gz"
   version "1.0.4"
   sha1 "ddf5463e5fa2304f401c88756551f75f65094826"
 
