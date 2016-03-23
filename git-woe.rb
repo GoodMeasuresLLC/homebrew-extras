09c2d109b0eaf1b5c20091f2190b67f25e9f7f6a
Robert Mathews <rob@justsoftwareconsulting.com>
2016-03-23 14:20:55 -0400

    updated git-woe for tagging issues

--- git-woe.rb
+++ git-woe.rb
@@ -1,8 +1,8 @@
 class GitWoe < Formula
   homepage ""
-  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.8.tar.gz"
-  version "1.0.8"
-  sha1 "a3e451ba4bcce7e5854fe941f184cada2d778c43"
+  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.0.9.tar.gz"
+  version "1.0.9"
+  sha1 "aa023552165f71d1108e66875d8522ea5af5a79e"
 
   def install
     bin.install "git-woe"
