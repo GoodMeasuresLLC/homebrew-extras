64e0b5c36fbc1705a96b5a622ba6304bfdd92b23
rob mathews <rob.mathews@goodmeasures.com>
2020-01-10 11:45:53 -0500

    I hate you github, that was so very unneeded

--- git-woe.rb
+++ git-woe.rb
@@ -1,8 +1,8 @@
 class GitWoe < Formula
   homepage ""
-  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.1.1.tar.gz"
-  version "1.1.1"
-  sha256 "541a89e37d1e3963aa6a7174892718b0c30e79dcf7d606d6009babead3bf53b6"
+  url "https://github.com/GoodMeasuresLLC/git-woe/archive/1.1.3.tar.gz"
+  version "1.1.3"
+  sha256 "8ea8f313ea0cb0470f0e31a0749192c11b1985b75ea077aae8519eb8b8474fd9"
 
   def install
     bin.install "git-woe"
